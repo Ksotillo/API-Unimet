9e0786750be8f615a9decaaf4e6285579ae30722
Kevin <ksotillo@gmail.com>
2019-09-11 01:56:06 -0400

    Settings and routes created

---
+++ settings.js
@@ -0,0 +1,14 @@
+const APIServerPort = 3000;
+
+const database = {
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'Apolos.2012',
+    database: 'CEDECA'
+}
+
+module.exports = {
+    database,
+    APIServerPort
+}
